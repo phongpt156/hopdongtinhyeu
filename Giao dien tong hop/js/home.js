99d54fb26a93dca94157fdaac56cbef88fbd87b3
Phong <pthanhphong156@gmail.com>
2018-10-03 21:18:59 +0700

    update trang 6

--- Giao dien tong hop/js/home.js
+++ Giao dien tong hop/js/home.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
   $('.aside aside').simpleScrollFollow();
+  SlideCrossFade = {
+    container: '.target-list .target-item',
+    displayTime: 5000,
+    fadeTime: 500,
+
+    start: function () {
+      const self = this;
+      $(this.container + ' img').show();
+      setInterval(function () {
+        self.next();
+      }, this.displayTime);
+    },
+
+    next: function () {
+      const self = this;
+
+      $(this.container).each(function () {
+        const $active = $(this).find('img.active');
+        const $next = ($active.next().length > 0) ? $active.next() : $(this).find('img').first();
+        $active.fadeOut(self.fadeTime, function () {
+          $active.removeClass('active');
+          $next.addClass('active');
+        });
+      });
+    }
+  };
+  SlideCrossFade.start();
 });
---
+++ Giao dien tong hop/js/wall.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  $('.updateAvt').css({
+    transition: 'inherit',
+    opacity: '1',
+    height: '52px',
+    background: 'rgba(6, 6, 6, 0.7)'
+  });
+  const intro = introJs();
+
+  intro.setOptions({
+    tooltipClass: 'custom-toolip',
+    hidePrev: true,
+    hideNext: true,
+    nextLabel: 'Tôi hiểu',
+    steps: [
+      {
+        element: '.updateAvt',
+        intro: `
+          <div>
+            <p>Click vào đây để cập nhật ảnh đại diện của bạn</p>
+            <p>Việc có ảnh đại diện sẽ tăng khả năng tương tác của bạn lên gấp 02 lần so với việc không có ảnh đại diện</p>
+            <div class="text-right">
+              <button class="btn btn-primary next-step">Tôi hiểu</button>
+            </div>
+          </div>
+        `
+      },
+      {
+        element: '.upload-post',
+        intro: `
+          <div>
+            <p>Click vào đây để đăng các thông tin theo một chủ đề chuyên mục</p>
+            <p>VD: Bạn muốn đăng hồ sơ kết bạn, bạn muốn đăng tin tuyển dụng, bạn muốn đăng hồ sơ ứng viên...</p>
+            <div class="text-right">
+              <button class="btn btn-primary next-step">Tôi hiểu</button>
+            </div>
+          </div>
+        `
+      },
+      {
+        element: '.upload-recruitment-post',
+        intro: `
+          <div>
+            <p>Click vào đây để đăng tin tuyển dụng nếu bạn có nhu cầu tuyển nhân viên cho một công việc nào đó</p>
+            <div class="text-right">
+              <button class="btn btn-primary next-step recruitment-next-step">Tôi hiểu</button>
+            </div>
+          </div>
+        `
+      },
+      {
+        element: '.update-cover',
+        intro: `
+          <div>
+            <p>Click vào đây để cập nhật ảnh bìa của bạn</p>
+            <p>Việc có bìa sẽ tăng khả năng tương tác của bạn lên gấp 02 lần so với việc không có ảnh bìa</p>
+            <div class="text-right">
+              <button class="btn btn-primary last-step">Tôi hiểu</button>
+            </div>
+          </div>
+        `
+      }
+    ]
+  });
+  intro.onbeforechange(function (targetElement) {
+    if ($(targetElement).hasClass('upload-recruitment-post')) {
+      $('.upload-post').tab('show');
+    }
+  });
+
+  $(document).on('click', '.next-step', function () {
+    intro.nextStep();
+  });
+  $(document).on('click', '.recruitment-next-step', function () {
+    $('.cover_changeBox .update-cover span').css('opacity', 1);
+    $('#upArticle').hide();
+  });
+  $(document).on('click', '.last-step', function () {
+    $('.cover_changeBox .update-cover span').css('opacity', 0);
+    $('.updateAvt').attr('style', '');
+    intro.exit();
+  });
+
+  intro.start();
+});
