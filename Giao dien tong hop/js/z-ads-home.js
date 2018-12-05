dd511b7f58bc6fe6b0e944c12a374d7429aef64f
Phong <pthanhphong156@gmail.com>
2018-12-05 21:36:32 +0700

    update trang 14

---
+++ Giao dien tong hop/js/z-ads-home.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.main').pagepiling({
+    sectionsColor: ['#000', '#000'],
+    navigation: {
+      'bulletsColor': '#777777',
+      'position': 'left',
+    }
+  });
+});
